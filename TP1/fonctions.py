73ca39e7e5732d1db09ae6db80c89e4994f7046a
Ewen-Ricou <ewen.ricou@univ-rouen.fr>
2023-09-06 16:47:48 +0200

    Modification fonction puissance

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,4 +1,7 @@
 def puissance(valeur,exposant):
+	carre = 0
 	if not type(valeur) is int:
 		raise TypeError("Only integers are allowed.")
-	return valeur**exposant
+	for loop in range(1,exposant):
+		carre+=valeur*valeur
+	return carre
