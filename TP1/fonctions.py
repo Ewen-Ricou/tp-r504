a663a7417200db4d17bac26a43ab053dd6cdc9f4
Ewen-Ricou <ewen.ricou@univ-rouen.fr>
2023-09-06 16:58:16 +0200

    Correction fonction puissance

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -2,7 +2,7 @@ def puissance(valeur,exposant):
 	carre = 0
 	if not type(valeur) is int:
 		raise TypeError("Only integers are allowed.")
-	for loop in range(1,exposant):
+	for loop in range(0,exposant-1):
 		carre+=valeur*valeur
 	if exposant == 0:
 		carre = 1
