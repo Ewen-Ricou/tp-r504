970683ebbacb0fcdb549dade4a094620a5b995d2
Ewen-Ricou <ewen.ricou@univ-rouen.fr>
2023-09-27 15:04:00 +0200

    Modif pour 0,0 et valeurs négatives

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,9 +1,14 @@
 def puissance(valeur,exposant):
-	puissance = 1
 	if not type(valeur) is int:
 		raise TypeError("Only integers are allowed.")
 	if not type(exposant) is int:
 		raise TypeError("Only integers are allowed.")
+	if valeur > 0 :
+		puissance = 1
+	elif valeur < 0 :
+		puissance = -1
+	else:
+		puissance = 0
 	for _ in range(exposant):
 		puissance *= valeur
 	return puissance
